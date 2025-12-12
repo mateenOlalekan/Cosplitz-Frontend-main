ddca1da99bbc49e664d8338ea64ba12af6f85117
mateenolalekan <matinolalek@gmail.com>
2025-12-12 17:28:41 +0100

    Work

--- src/pages/Auth/Register/EmailVerificationStep.jsx
+++ src/pages/Auth/Register/EmailVerificationStep.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React,{ useState, useEffect } from "react";
 import { authService } from "../../../services/api";
 import { ArrowLeft, Mail } from "lucide-react";
 
