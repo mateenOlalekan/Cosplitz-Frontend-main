9bea6c97dbc1d2cb0334eda27399b9b4e1bb27ab
mateenolalekan <matinolalek@gmail.com>
2025-12-12 13:30:40 +0100

    justwork

--- src/pages/Auth/ForgotPass.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ForgotPass() {
-  return (
-    <div>ForgotPass</div>
-  )
-}
-
-export default ForgotPass
--- src/pages/Auth/PasswordReset.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PasswordReset() {
-  return (
-    <div>PasswordReset</div>
-  )
-}
-
-export default PasswordReset
