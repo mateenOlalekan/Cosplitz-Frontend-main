3bddb88eed911d50505a02f1954755de019e026c
mateenolalekan <matinolalek@gmail.com>
2025-12-15 20:09:51 +0100

    767

--- src/pages/Auth/Register/index.jsx
+++ src/pages/Auth/Register/index.jsx
@@ -195,7 +195,7 @@ export default function Register() {
       setCurrentStep(3);
       
       setTimeout(() => {
-        navigate("/login");
+        navigate("/dashboard");
       }, 2000);
     }
   };
